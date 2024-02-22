522762fe68d8e1ae3b1e0d40f4348b2d121cb83e
Joonas Tuominen <joonas.o.tuominen@outlook.com>
2024-02-22 20:18:08 +0200

    Workout combines steps and repeats

--- fit_file_interfacing/fit_interface/src/main/java/com/github/JoonasOT/Training/Workout.java
+++ fit_file_interfacing/fit_interface/src/main/java/com/github/JoonasOT/Training/Workout.java
@@ -3,16 +3,34 @@ package com.github.JoonasOT.Training;
 import com.garmin.fit.*;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Workout {
     private WorkoutHeader header;
     private WorkoutMesg workoutDescription;
-    private ArrayList<WorkoutStep> steps;
+    private ArrayList<WorkoutStepMesg> steps;
     private Workout(WorkoutBuilder builder) {
         header = builder.workoutHeader;
         workoutDescription = builder.workoutDescription;
-        steps = builder.workoutSteps;
+        steps = WorkoutStepMesgFactory(builder.workoutSteps, builder.workoutRepeats);
     }
+
+    private ArrayList<WorkoutStepMesg> WorkoutStepMesgFactory(ArrayList<WorkoutStep> steps,
+                                                              ArrayList<WorkoutRepeats> reps) {
+        Map<Integer, WorkoutRepeats> repeatLocations = reps.stream()
+                                                            .collect(Collectors.toMap(WorkoutRepeats::getEnd, i -> i));
+        ArrayList<WorkoutStepMesg> result = new ArrayList<>();
+        for(int i = 0; i < steps.size(); i++) {
+            result.add(steps.get(i).GetWorkoutStep());
+            if (repeatLocations.containsKey(i)) {
+                result.add(repeatLocations.get(i).CreateWorkoutStepRepeat());
+            }
+        }
+        return result;
+    }
+
     public static class WorkoutBuilder {
 
         // The workout requires a header and description
@@ -21,25 +39,30 @@ public class Workout {
 
         // The workout may contain actual data as well
         private ArrayList<WorkoutStep> workoutSteps;
-
+        private ArrayList<WorkoutRepeats> workoutRepeats;
+        private int size;
         public WorkoutBuilder(WorkoutHeader header, String workoutName) {
             workoutHeader = header;
             workoutSteps = new ArrayList<>();
+            workoutRepeats = new ArrayList<>();
 
             workoutDescription = new WorkoutMesg();
             workoutDescription.setWktName(workoutName);
             workoutDescription.setSport(Sport.CYCLING);
             workoutDescription.setSubSport(SubSport.INVALID);
-            workoutDescription.setNumValidSteps(workoutSteps.size());
+            workoutDescription.setNumValidSteps(calcSize());
+        }
+        private int calcSize() {
+            return (size = workoutSteps.size() + workoutRepeats.size());
         }
         public WorkoutBuilder addStep(WorkoutStep step) {
             workoutSteps.add(step);
-            workoutDescription.setNumValidSteps(workoutSteps.size());
+            workoutDescription.setNumValidSteps(calcSize());
             return this;
         }
-        public WorkoutBuilder addRepeat(WorkoutStep repeat) {
-            workoutSteps.add(repeat);
-            workoutDescription.setNumValidSteps(workoutSteps.size());
+        public WorkoutBuilder addRepeat(WorkoutRepeats repeat) {
+            workoutRepeats.add(repeat);
+            workoutDescription.setNumValidSteps(calcSize());
             return this;
         }
         public Workout build() {
@@ -60,6 +83,8 @@ public class Workout {
 
         // Write the messages to the file, in the proper sequence
         encode.write(header.getHeader());
-        encode.write(header.getWorkoutDescription());
+        encode.write(workoutDescription);
+
+
     }
 }
