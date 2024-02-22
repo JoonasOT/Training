5f893050cbb08d85a870d07208391f9112d12dc4
Joonas Tuominen <joonas.o.tuominen@outlook.com>
2024-02-22 21:21:48 +0200

    Update WorkoutTest.java
    Added test to interval creation

--- fit_file_interfacing/fit_interface/src/test/java/com/github/JoonasOT/Training/WorkoutTest.java
+++ fit_file_interfacing/fit_interface/src/test/java/com/github/JoonasOT/Training/WorkoutTest.java
@@ -1,20 +1,22 @@
 package com.github.JoonasOT.Training;
 
-import com.garmin.fit.*;
-import com.garmin.fit.csv.CSVTool;
-import com.garmin.fit.plugins.HrToRecordMesgBroadcastPlugin;
 import org.junit.jupiter.api.Test;
 
-import java.io.FileInputStream;
+import com.garmin.fit.*;
+import com.garmin.fit.csv.CSVTool;
+
 
 public class WorkoutTest {
     @Test
     void WorkoutHeaderCreationAndConversion() {
+        System.out.println("Creating workout header.");
         WorkoutHeader wh = new WorkoutHeader("filename");
-        wh.getHeader();
+
+        System.out.println("Got header: " + wh.getHeader().toString());
     }
     @Test
     void WorkoutStepCreationAndConversion() {
+        System.out.println("Creating workout steps with target value and custom values.");
         WorkoutStep tv = new WorkoutStep(0, "Step 0", "notes 0",
                                         Intensity.ACTIVE, WktStepDuration.TIME,
                                         100*60*60, WktStepTarget.POWER, 3);
@@ -22,13 +24,13 @@ public class WorkoutTest {
         WorkoutStep cv = new WorkoutStep(1, "Step 1", "notes 1",
                 Intensity.ACTIVE, WktStepDuration.TIME,
                 100*60*60, WktStepTarget.POWER, 1125, 1250);
-
-        tv.GetWorkoutStep();
-        cv.GetWorkoutStep();
+        System.out.println("Formed the target value step " + tv.GetWorkoutStep().toString() + " and " +
+                "completely custom valued step " + cv.GetWorkoutStep().toString());
     }
+
     @Test
-    void WorkoutCreationAndSave() {
-        String file = "WorkoutTestResult.fit";
+    void BasicWorkoutCreationAndSave() {
+        String file = "BasicWorkoutTestResult.fit";
         WorkoutStep tv = new WorkoutStep(0, "Step 0", "notes 0",
                 Intensity.ACTIVE, WktStepDuration.TIME,
                 100*60*60, WktStepTarget.POWER, 3);
@@ -39,6 +41,32 @@ public class WorkoutTest {
 
         Workout workout = new Workout.WorkoutBuilder(new WorkoutHeader("filename"), "test")
                                      .addStep(tv).addStep(cv).build();
+        workout.createFile(file);
+
+        CSVTool.main(new String[]{"-b", file, file.substring(0, file.length() - 4) + ".csv"});
+    }
+
+    @Test
+    void WorkoutCreationAndSave() {
+        String file = "WorkoutTestResult.fit";
+        WorkoutStep warmup = new WorkoutStep(0, "Warm-up", null,
+                Intensity.WARMUP, WktStepDuration.TIME,
+                100*60*10, WktStepTarget.POWER, 1);
+
+        WorkoutStep heavy = new WorkoutStep(1, "Stomp", "As hard as possible!!",
+                Intensity.INTERVAL, WktStepDuration.TIME,
+                100*20, WktStepTarget.POWER, 2000, 2250);
+        WorkoutStep recovery = new WorkoutStep(2, "Recover", null,
+                Intensity.RECOVERY, WktStepDuration.TIME,
+                100*60*3, WktStepTarget.POWER, 1);
+        WorkoutRepeats repeat = new WorkoutRepeats(3, 1, 2, 6);
+
+        WorkoutStep cooldown = new WorkoutStep(4, "Cooldown", null,
+                Intensity.RECOVERY, WktStepDuration.TIME,
+                100*60*3, WktStepTarget.POWER, 1);
+
+        Workout workout = new Workout.WorkoutBuilder(new WorkoutHeader("filename"), "test")
+                .addStep(warmup).addStep(heavy).addStep(recovery).addRepeat(repeat).addStep(cooldown).build();
         workout.createFile(file);
 
         CSVTool.main(new String[]{"-b", file, file.substring(0, file.length() - 4) + ".csv"});
