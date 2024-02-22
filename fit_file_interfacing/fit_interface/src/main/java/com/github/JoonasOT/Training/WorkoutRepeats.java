84181e1338b9d47b72a8709575275c6aa3e3eae8
Joonas Tuominen <joonas.o.tuominen@outlook.com>
2024-02-22 20:17:40 +0200

    Update WorkoutRepeats.java

--- fit_file_interfacing/fit_interface/src/main/java/com/github/JoonasOT/Training/WorkoutRepeats.java
+++ fit_file_interfacing/fit_interface/src/main/java/com/github/JoonasOT/Training/WorkoutRepeats.java
@@ -1,4 +1,35 @@
 package com.github.JoonasOT.Training;
 
+import com.garmin.fit.WktStepDuration;
+import com.garmin.fit.WktStepTarget;
+import com.garmin.fit.WorkoutStepMesg;
+
 public class WorkoutRepeats {
+    private int msgIndex;
+    private int repeatFrom;
+    private int repeatEnd;
+    private int repetitions;
+    public WorkoutRepeats(int messageIndex, int repeatFrom, int repeatEnd, int repetitions) {
+        msgIndex = messageIndex;
+        this.repeatFrom = repeatFrom;
+        this.repeatEnd = repeatEnd;
+        this.repetitions = repetitions;
+    }
+
+    public int getEnd() {
+        return repeatEnd;
+    }
+
+    public WorkoutStepMesg CreateWorkoutStepRepeat() {
+        WorkoutStepMesg workoutStepMesg = new WorkoutStepMesg();
+        workoutStepMesg.setMessageIndex((msgIndex));
+
+        workoutStepMesg.setDurationType(WktStepDuration.REPEAT_UNTIL_STEPS_CMPLT);
+        workoutStepMesg.setDurationValue((long) repeatFrom);
+
+        workoutStepMesg.setTargetType(WktStepTarget.OPEN);
+        workoutStepMesg.setTargetValue((long) repetitions);
+
+        return workoutStepMesg;
+    }
 }
